d5dd871d1f1b3e95e14f3d572eb97bef07571f60
cheungky <kycheung1985@gmail.com>
2021-02-21 15:12:47 +0800

    Add Login service

--- demo_jsp/src/main/java/com/example/demo_jsp/controller/LoginController.java
+++ demo_jsp/src/main/java/com/example/demo_jsp/controller/LoginController.java
@@ -1,13 +1,19 @@
 package com.example.demo_jsp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo_jsp.service.LoginService;
+
 @Controller
 public class LoginController {
+	
+	@Autowired
+	LoginService service;
 	
 	@RequestMapping(value="/login",method=RequestMethod.GET)
 	public String showLoginPage(ModelMap model) {
@@ -18,6 +24,12 @@ public class LoginController {
 	@RequestMapping(value="/login",method=RequestMethod.POST)
 	public String showWelcomePage(ModelMap model,@RequestParam String name,@RequestParam String password) {
 		
+		boolean isValidUser=service.validateUser(name,password);
+		
+		if(!isValidUser) {
+			model.put("errorMessage", "Invalid Credentials");
+			return "login";
+		}
 		
 		return "welcome";
 	}
--- demo_jsp/src/main/webapp/WEB-INF/jsp/login.jsp
+++ demo_jsp/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -8,7 +8,11 @@
     <font color="red">${errorMessage}</font>
     <form method="post">
         Name : <input type="text" name="name" />
+        <br>
+        <br>
         Password : <input type="password" name="password" /> 
+        <br>
+        <br>
         <input type="submit" />
     </form>
 </body>
