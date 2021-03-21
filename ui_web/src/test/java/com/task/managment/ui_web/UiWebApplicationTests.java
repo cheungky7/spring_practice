b2f566a5119ff103804d803469fa348507f29d63
cheungky <kycheung1985@gmail.com>
2021-03-21 22:54:52 +0800

    Modify tests

--- ui_web/src/test/java/com/task/managment/ui_web/UiWebApplicationTests.java
+++ ui_web/src/test/java/com/task/managment/ui_web/UiWebApplicationTests.java
@@ -7,13 +7,15 @@ package com.task.managment.ui_web;
 //import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mybatis.spring.annotation.MapperScan;
+//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.task.management.ui_web.UiWebApplication;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UiWebApplication.class)
 class UiWebApplicationTests {
 
 	@Test
